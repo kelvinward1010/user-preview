78190e04c38b56fd0f24ea34478a5ab355c64906
Kelvin Ward <kelvinward1010@gmail.com>
2024-08-12 16:34:11 +0700

    update config base

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-})
+// eslint.config.js
+export default [
+  {
+      extends: [
+          "plugin:prettier/recommended",
+          "prettier"
+      ],
+      plugins: ['prettier'],
+      rules: {
+          semi: "error",
+          "prefer-const": "error",
+          "prettier/prettier": ["warn"]
+      }
+  }
+];
